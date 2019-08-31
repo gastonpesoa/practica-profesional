1f738358ec5d3459510ac0d3e24ad71e892c9f8a
gastonpesoa <gastonpesoa@gmail.com>
2019-08-30 23:53:20 -0300

    chat-ionic

---
+++ ionic-chat/chat-tutorial/src/app/servicios/auth.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth'
+import { resolve } from 'url';
+import { reject } from 'q';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private AFauth: AngularFireAuth) { }
+
+  login(email: string, password: string){
+
+    return new Promise((resolve, reject) => {
+
+      this.AFauth.auth.signInWithEmailAndPassword(email, password)
+        .then(user => {
+          resolve(user);
+        })
+        .catch(err => {
+          reject(err);
+        });
+    })
+  }
+}
---
+++ ionic-chat/chat-tutorial/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false
+};
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAwXiIq_8WcUDIOZONViP6KNkssUc5xkQY",
+  authDomain: "chat-tutorial-a2ea5.firebaseapp.com",
+  databaseURL: "https://chat-tutorial-a2ea5.firebaseio.com",
+  projectId: "chat-tutorial-a2ea5",
+  storageBucket: "",
+  messagingSenderId: "111408055414",
+  appId: "1:111408055414:web:51c99a9a921ab356"
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
